0f1e3824fabc21d8d2e2c89e4a4974abe6b3f0db
Priyanka <original.manaria@gmail.com>
2022-01-19 12:31:11 +0530

    this is my iNotebook

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,20 @@
-const connectToMongo = require('./db');
-const express = require('express')
-var cors = require('cors') 
+const connectToMongo = require("./db");
+const express = require("express");
+var cors = require("cors");
 
 connectToMongo();
-const app = express()
-const port = 5000
+const app = express();
+const port = 5000;
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // Available Routes
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/notes', require('./routes/notes'))
-
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes"));
 
 app.listen(port, () => {
-  console.log(`iNotebook backend listening at http://localhost:${port}`)
-})
+  console.log(
+    `iNotebook backend listening at http://localhost:${port}`
+  );
+});
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -20,20 +20,22 @@ router.post(
     ).isLength({ min: 5 }),
   ],
   async (req, res) => {
+    let success = false;
     // If there are errors, return Bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res
+        .status(400)
+        .json({ success, errors: errors.array() });
     }
     try {
       // Check whether the user with this email exists already
       let user = await User.findOne({ email: req.body.email });
       if (user) {
-        return res
-          .status(400)
-          .json({
-            error: "Sorry a user with this email already exists",
-          });
+        return res.status(400).json({
+          success,
+          error: "Sorry a user with this email already exists",
+        });
       }
       const salt = await bcrypt.genSalt(10);
       const secPass = await bcrypt.hash(req.body.password, salt);
@@ -52,7 +54,8 @@ router.post(
       const authtoken = jwt.sign(data, JWT_SECRET);
 
       // res.json(user)
-      res.json({ authtoken });
+      success = true;
+      res.json({ success, authtoken });
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Internal Server Error");
@@ -80,11 +83,9 @@ router.post(
       let user = await User.findOne({ email });
       if (!user) {
         success = false;
-        return res
-          .status(400)
-          .json({
-            error: "Please try to login with correct credentials",
-          });
+        return res.status(400).json({
+          error: "Please try to login with correct credentials",
+        });
       }
 
       const passwordCompare = await bcrypt.compare(
@@ -93,12 +94,10 @@ router.post(
       );
       if (!passwordCompare) {
         success = false;
-        return res
-          .status(400)
-          .json({
-            success,
-            error: "Please try to login with correct credentials",
-          });
+        return res.status(400).json({
+          success,
+          error: "Please try to login with correct credentials",
+        });
       }
 
       const data = {
